d7b11f63b338a7e932f89454c8d1b65f3b2e18f5
mmohanapriyapriya68-blip <mmohanapriyapriya68@gmail.com>
2026-04-25 19:11:48 +0530

    exp1b.py
    This script demonstrates various array operations using NumPy, including addition, multiplication, scalar multiplication, square root, mean, and dot product.

---
+++ array operation.py
@@ -0,0 +1,11 @@
+import numpy as np
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6]) print(
+"Array a:", a, "\n"
+"Array b:", b, "\n\n"
+"Addition (a + b):", a + b, "\n" "Multiplication (a * b):", a * b, "\n"
+"Scalar Multiplication (a * 10):", a * 10, "\n\n" "Square Root of a:", np.sqrt(a),
+"\n"
+"Mean of a:", np.mean(a), "\n"
+"Dot Product (a · b):", np.dot(a, b)
+)
